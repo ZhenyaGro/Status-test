552a98aa2ea0f750df6fc8791f4ff13f408bb015
ZhenyaGro <bon4ios@gmail.com>
2023-10-14 22:17:31 +0300

    Change folder name

--- src/tests/expect.ts
+++ src/tests/expect.ts
@@ -1,4 +1,4 @@
-module.exports = {
+export const items = {
   test1: [{ "id": 1, "parent": "root" }, { "id": 2, "parent": 1, "type": "test" }, { "id": 3, "parent": 1, "type": "test" }, { "id": 4, "parent": 2, "type": "test" }, { "id": 5, "parent": 2, "type": "test" }, { "id": 6, "parent": 2, "type": "test" }, { "id": 7, "parent": 4, "type": null }, { "id": 8, "parent": 4, "type": null }],
   test2: { "id": 7, "parent": 4, "type": null },
   test3: [{ "id": 7, "parent": 4, "type": null }, { "id": 8, "parent": 4, "type": null }],
